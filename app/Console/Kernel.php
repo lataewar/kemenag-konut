f590d4e1ca1f4e81b9aa78cfcb03b55692146018
lataewar <black.lst23@gmail.com>
2024-03-11 23:25:38 +0800

    prod 2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Repositories\TemporaryFileRepository;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,8 +14,12 @@ class Kernel extends ConsoleKernel
    */
   protected function schedule(Schedule $schedule): void
   {
-    $schedule->command('app:clear-temp')->everyTenMinutes();
-    // $schedule->command('inspire')->hourly();
+    // laravel command cannot be used on shared hosting server because proc_open php function is not active
+    // $schedule->command('app:clear-temp')->everyTenMinutes();
+    $schedule->call(function () {
+      app(TemporaryFileRepository::class)->deleteTemporary();
+      Log::info('Scheduler clear temporary file berhasil dijalankan');
+    })->everyDay();
   }
 
   /**
